e8a75868ca7776973681f42d97181ebc43e99cbd
Aooobi <zzzzhururu123@163.com>
2026-01-22 22:25:49 +0800

    扩展Attribute

--- Assets/Scripts/Attribute.cs
+++ Assets/Scripts/Attribute.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Attribute : MonoBehaviour
 {
+    public string name;
+    public string description;
+    public string type;
     // Start is called before the first frame update
     void Start()
     {
