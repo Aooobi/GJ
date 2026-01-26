c693381defb2e1bc4e2a6e48b319f7f6730eaebe
B-ender <2918248793@qq.com>
2026-01-26 21:46:24 +0800

    抢救1

--- Assets/Editor/ItemBaseEditor.cs
+++ Assets/Editor/ItemBaseEditor.cs
@@ -5,11 +5,12 @@ using UnityEditor;
 public class ItemBaseEditor : Editor {
 
     private SerializedObject itemObject;//序列化对象
-    private SerializedProperty nameProp, icon, description, maxStack, itemType, damage, health, function;//定义各种属性
+    private SerializedProperty id, nameProp, icon, description, maxStack, itemType, damage, health, function, additionalData, functionScript;//定义各种属性
 
     void OnEnable()
     {
         itemObject = new SerializedObject(target);
+        id = itemObject.FindProperty("id");
         nameProp = itemObject.FindProperty("name");
         icon = itemObject.FindProperty("icon");
         description = itemObject.FindProperty("description");
@@ -18,20 +19,25 @@ public class ItemBaseEditor : Editor {
         damage = itemObject.FindProperty("damage");
         health = itemObject.FindProperty("health");
         function = itemObject.FindProperty("function");
+        additionalData = itemObject.FindProperty("additionalData");
+        functionScript = itemObject.FindProperty("functionScript");
     }
 
     public override void OnInspectorGUI()
     {
         itemObject.Update();//更新对象
 
+        EditorGUILayout.PropertyField(id, new GUIContent("唯一标识物品ID"));
         EditorGUILayout.PropertyField(nameProp, new GUIContent("物品名称"));
         EditorGUILayout.PropertyField(icon, new GUIContent("物品图标"));
         EditorGUILayout.PropertyField(description, new GUIContent("物品描述"));
         EditorGUILayout.PropertyField(maxStack, new GUIContent("堆叠数量"));
+        EditorGUILayout.PropertyField(additionalData, new GUIContent("附加数据"));
+        EditorGUILayout.PropertyField(functionScript, new GUIContent("功能脚本"));
         EditorGUILayout.PropertyField(itemType, new GUIContent("物品类型"));
 
         // 根据选择的枚举类型显示不同的字段
-        if (itemType.enumValueIndex == (int)ItemBase.ItemType.装备) { // 装备显示血量
+        if (itemType.enumValueIndex == (int)ItemBase.ItemType.饰品) { // 饰品显示血量
             EditorGUILayout.PropertyField(health, new GUIContent("血量"));
         }
         else if (itemType.enumValueIndex == (int)ItemBase.ItemType.武器) { // 武器显示伤害
@@ -40,8 +46,12 @@ public class ItemBaseEditor : Editor {
         else if (itemType.enumValueIndex == (int)ItemBase.ItemType.道具) { // 道具显示功能
             EditorGUILayout.PropertyField(function, new GUIContent("功能"));
         }
+        else if (itemType.enumValueIndex == (int)ItemBase.ItemType.消耗品) { // 消耗品显示功能
+            EditorGUILayout.PropertyField(function, new GUIContent("功能"));
+        }
         // 其他类型不显示额外内容
-
+        
         itemObject.ApplyModifiedProperties();//应用修改
+        
     }
 }
