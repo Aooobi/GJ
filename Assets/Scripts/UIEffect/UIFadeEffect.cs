09131f5e570632206286f5eb9824ec8964788b6b
Aooobi <zzzzhururu123@163.com>
2026-01-26 21:16:30 +0800

    更新场景和脚本
    制作了新的场景  加入了UIEffectFade脚本和TeleportZone传送门脚本

---
+++ Assets/Scripts/UIEffect/UIFadeEffect.cs
@@ -0,0 +1,79 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIFadeEffect : MonoBehaviour
+{
+    public static UIFadeEffect Instance;
+    [Header("黑屏图片")]
+    public Image Black;
+    [Header("时间参数")]
+    [SerializeField]private float fade_duration = 1f;
+    [SerializeField] private float stop_duration = 1f;
+
+    [Header("需要显隐控制的面板")]
+    public GameObject TargetPanel;
+    //[Header("黑屏时触发的事件")]
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        //测试一下 淡入淡出
+        //FadeOutAndFadeIn(() => { }, () => { }, false);
+       
+
+    }
+    #region 单次方法
+    //淡出屏幕渐黑 单次方法
+    public void FadeOut()
+    {
+        TargetPanel.gameObject.SetActive(false);
+        Black.DOFade(1, fade_duration).SetEase(Ease.Linear);
+    }
+    //淡入屏幕渐亮 单次方法
+    public void FadeIn()
+    {
+        Black.DOFade(0, fade_duration).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            TargetPanel.gameObject.SetActive(true);
+        });
+    }
+    #endregion
+
+
+    public void FadeOutAndFadeIn(Action on_black, Action on_complete = null,bool active_panel_on_end = true)
+    {
+        if (on_black == null) return;
+        if (TargetPanel != null)
+            TargetPanel.gameObject.SetActive(false);
+        Black.DOFade(1, fade_duration).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            on_black?.Invoke();
+
+            transform.DOMove(transform.position, stop_duration).OnComplete(() =>
+            {
+                Black.DOFade(0, fade_duration).SetEase(Ease.Linear).OnComplete(() =>
+                {
+                    if (TargetPanel != null)
+                        TargetPanel.gameObject.SetActive(active_panel_on_end);
+                    //结束委托调用
+                    on_complete?.Invoke();
+                });
+
+            });
+        });
+    }
+}
