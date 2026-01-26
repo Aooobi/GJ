ecd5c980e3955879600d3c93c53f44caf8dc6b69
lingnine1008 <3154952946@qq.com>
2026-01-26 15:37:39 +0800

    完成最基本的村民npc移动逻辑脚本
    完成最基本的村民巡逻
    需要继续完成/优化：
    1.遇到主角停下来的功能
    2.村民之间的碰撞  这个是直接穿透比较好 还是遇到同类就转弯（这个观感应该不好）

---
+++ Assets/Scripts/Base/NPC.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 村民npc移动脚本  基础的移动   衡玉 
+/// </summary>
+public class NPC : MonoBehaviour
+{
+    [Header("村民移动")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private Transform villageleft;
+    [SerializeField] private Transform villageright;
+    private Vector2 targetPoint;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Transform player;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        if(rb == null)
+        {
+            Debug.Log("NPC未挂载Rigidbody2D");
+            return;
+        }
+
+    }
+
+    private void Start()
+    {
+        RandomPoint();
+
+    }
+
+    private void Update()
+    {
+        NPCpatrol();
+
+
+    }
+
+    #region 巡逻
+    private void NPCpatrol()
+    {
+        //计算方向
+        Vector2 direction = (targetPoint - (Vector2)transform.position).normalized;
+        //移动
+        rb.velocity = new Vector2(direction.x * moveSpeed,rb.velocity.y);
+        //重新随机目标点
+        if(Vector2.Distance(transform.position,targetPoint) < 0.1f)
+        {
+            RandomPoint();
+
+        }
+
+    }
+
+    private void RandomPoint()
+    {
+        if (villageleft != null && villageright !=null)
+        {
+            float maxX = Mathf.Max(villageleft.position.x,villageright.position.x);
+            float minX = Mathf.Min(villageleft.position.x,villageright.position.x);
+
+            float randomX = Random.Range(minX,maxX);
+
+            targetPoint = new Vector2(randomX, villageleft.position.y);
+
+            Debug.Log("村民继续巡逻");
+
+        }
+        else
+        {
+            Debug.Log("村民左右巡逻点未关联");
+
+        }
+
+
+
+    }
+
+
+
+    #endregion
+}
