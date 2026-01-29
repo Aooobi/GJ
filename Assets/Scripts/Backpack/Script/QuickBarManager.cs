89dab8663a96dde55188e8111eeac1b232b1553b
B-ender <2918248793@qq.com>
2026-01-29 17:42:31 +0800

    BP 完整

--- Assets/Scripts/Backpack/Script/QuickBarManager.cs
+++ Assets/Scripts/Backpack/Script/QuickBarManager.cs
@@ -53,6 +53,16 @@ public class QuickBarManager : MonoBehaviour
         RefreshImage();
     }
 
+    public static void DropItem(ItemBase item) {
+        if (instance.QuickBar.itemList.Contains(item)) {
+            instance.QuickBar.itemList.Remove(item);
+        }
+        else {
+            print("物品不存在");
+        }
+        RefreshImage();
+    }
+
     public static void RefreshImage() {
         for (int i = 0; i < SlotNum; i++) {
             instance.Slots[i].slotImage.sprite = instance.DefaultIcon;
@@ -65,7 +75,58 @@ public class QuickBarManager : MonoBehaviour
         }
     }
 
-    public static void UpdateItemInfo(string info) {
+    public static void UseItem(int i) {
+        var attrObj = FindObjectOfType<CharacterStats>();
+        var item = instance.QuickBar.itemList[i];
+        if (instance.QuickBar.itemList[i].itemHeld == 1) {
+            DropItem(item);
+        }
+        else {
+            instance.QuickBar.itemList[i].itemHeld--;
+        }
+        if (attrObj != null && item != null) {
+            // 根据物品类型和属性值来增加角色属性
+            //
+            // // 如果物品有生命值加成
+            // if (item.health != 0) {
+            //     attrObj.WearAdd(AddType.MaxHealth, item.health, true);
+            // }
+            //
+            // // 如果物品有攻击力加成（轻攻击）
+            // if (item.damage != 0) {
+            //     attrObj.WearAdd(AddType.LightAttack, item.damage, true);
+            // }
+            //
+            // // 如果物品有防御力加成
+            // if (item.Defense != 0) {
+            //     attrObj.WearAdd(AddType.Defense, item.Defense, true);
+            // }
+            //
+            // // 如果物品有速度加成
+            // if (item.Speed != 0) {
+            //     attrObj.WearAdd(AddType.MoveSpeed, item.Speed, true);
+            // }
+            
+            // 如果物品有持续时间，则应用临时效果
+            if (item.Duration > 0) {
+                if (item.health != 0) {
+                    attrObj.WearAdd(AddType.CurrentHealth, item.health, false, item.Duration);
+                }
+                
+                if (item.damage != 0) {
+                    attrObj.WearAdd(AddType.LightAttack, item.damage, false, item.Duration);
+                }
+                
+                if (item.Defense != 0) {
+                    attrObj.WearAdd(AddType.Defense, item.Defense, false, item.Duration);
+                }
+                
+                if (item.Speed != 0) {
+                    attrObj.WearAdd(AddType.MoveSpeed, item.Speed, false, item.Duration);
+                }
+            }
+        }
         
+        RefreshImage();
     }
 }
