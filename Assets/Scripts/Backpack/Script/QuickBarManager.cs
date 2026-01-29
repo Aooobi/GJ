4094ed11e7f91319f9dc9b65055325e188964ec0
B-ender <2918248793@qq.com>
2026-01-29 16:35:43 +0800

    BP13

--- Assets/Scripts/Backpack/Script/QuickBarManager.cs
+++ Assets/Scripts/Backpack/Script/QuickBarManager.cs
@@ -9,6 +9,7 @@ public class QuickBarManager : MonoBehaviour
     public Backpack Bag;
     public QuickBar QuickBar;
     public QuickBar_Slot[] Slots;
+    public Sprite DefaultIcon;
     public static int SlotNum = 3;
     
     void Awake() {
@@ -24,8 +25,11 @@ public class QuickBarManager : MonoBehaviour
 
     public static void AddItemToQuickBar(ItemBase item) {
         if (instance.QuickBar.itemList.Count < SlotNum) {
-            if (!instance.QuickBar.itemList.Contains(item))
-            instance.QuickBar.itemList.Add(item);
+            if (!instance.QuickBar.itemList.Contains(item)) {
+                instance.QuickBar.itemList.Add(item);
+                instance.Bag.itemList.Remove(item);
+                BPManager.RefreshItem();
+            }
             else {
                 print("物品已存在"); //写成通知提醒
             }
@@ -38,8 +42,11 @@ public class QuickBarManager : MonoBehaviour
     }
 
     public static void RemoveItemFromQuickBar(ItemBase item) {
-        if (instance.QuickBar.itemList.Contains(item))
+        if (instance.QuickBar.itemList.Contains(item)) {
             instance.QuickBar.itemList.Remove(item);
+            instance.Bag.itemList.Add(item);
+            BPManager.RefreshItem();
+        }
         else {
             print("物品不存在");
         }
@@ -47,9 +54,18 @@ public class QuickBarManager : MonoBehaviour
     }
 
     public static void RefreshImage() {
+        for (int i = 0; i < SlotNum; i++) {
+            instance.Slots[i].slotImage.sprite = instance.DefaultIcon;
+            instance.Slots[i].Number.text = "00";
+        }
         for (int i = 0; i < instance.QuickBar.itemList.Count; i++) {
             instance.Slots[i].slotImage.sprite = instance.QuickBar.itemList[i].icon;
             instance.Slots[i].itemOnslot = instance.QuickBar.itemList[i];
+            instance.Slots[i].Number.text = instance.QuickBar.itemList[i].itemHeld.ToString();
         }
+    }
+
+    public static void UpdateItemInfo(string info) {
+        
     }
 }
